2b5849d3a8c65c6a57f8f71920b41e5f48fbecff
Meet Shah <meet1995@gmail.com>
2015-09-13 14:31:19 +0530

    Modified ini

--- hackinout/production.py
+++ hackinout/production.py
@@ -92,11 +92,12 @@ WSGI_APPLICATION = 'hackinout.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/home/inout/hackinout/db.cnf',
+        },
     }
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
