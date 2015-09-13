b5a29db968847cbaea25c255c2891aac77fc805c
Meet Shah <meet1995@gmail.com>
2015-09-13 14:34:30 +0530

    Fourth commit

--- inout/admin.py
+++ inout/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import InoutUser,InoutUserLinks
+from .models import InoutUser,InoutUserLink
 
 #admin.site.register(InoutUser)
-admin.site.register(InoutUserLinks)
+admin.site.register(InoutUserLink)
 @admin.register(InoutUser)
 class ParticipantAdmin(admin.ModelAdmin):
     list_display = ('full_name','application_status','email')
