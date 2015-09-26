c0f1ab2f55d796573a3478294186198a238d28eb
Meet Shah <meet1995@gmail.com>
2015-09-26 12:37:19 +0530

    piwik changes

--- inout/views.py
+++ inout/views.py
@@ -26,6 +26,9 @@ from inout.forms import InoutUserForm
 
 from django.contrib.auth.decorators import login_required
 
+from django.views.decorators.clickjacking import xframe_options_exempt
+
+@xframe_options_exempt
 def Index(request):
     user=request.user
     if user.is_superuser:
@@ -39,6 +42,7 @@ def Index(request):
     inout_user_status=user.inoutuser.application_status
     return render(request,'inout/index.html',{'user':request.user,'app_status':inout_user_status})
 
+@xframe_options_exempt
 @login_required
 def UserDash(request):
     user=request.user
