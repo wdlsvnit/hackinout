8079c5f3488a4941753293b195801339afc4c919
vikas-parashar <svnitvikas@gmail.com>
2015-09-29 00:11:09 +0530

    linkedin link clickable

--- inout/admin.py
+++ inout/admin.py
@@ -16,13 +16,16 @@ class ParticipantAdmin(admin.ModelAdmin):
 
 @admin.register(InoutUserLink)
 class ProfileAdmin(DjangoObjectActions,admin.ModelAdmin):
-    list_display = ('inout_user','approval_status','github_link','linkedin_account','city','resume','additional_info')
+    list_display = ('inout_user','approval_status','github_link','linkedin_link','city','resume','additional_info')
 
     def github_link(self, obj):
     	return format_html("<a target='_blank' href='{url}'>{url}</a>", url=obj.github_account)
+    def linkedin_link(self, obj):
+    	return format_html("<a target='_blank' href='{url}'>{url}</a>", url=obj.linkedin_account)
 
     github_link.allow_tags = True
-    
+    linkedin_link.allow_tags = True
+
     def approval_status(self,obj):
         return obj.inout_user.application_status
     approval_status.allow_tags = True
