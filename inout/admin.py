08e6dc32fa0859004c5c56b4a4843232579cd0ce
Meet Shah <meet1995@gmail.com>
2015-10-16 00:27:12 +0530

    Djrill

--- inout/admin.py
+++ inout/admin.py
@@ -40,27 +40,26 @@ class ProfileAdmin(DjangoObjectActions,admin.ModelAdmin):
         applicant.application_status = True
         applicant.save()
 
-        # first_name = applicant.first_name
-        # subject = 'InOut Registration approved!'
-        # from_email = settings.DEFAULT_FROM_EMAIL
-        # to_email = [ applicant.email ]
-        # context = {
-        #     "first_name": first_name,
-        # }
-        # email_template_html = "mail_body.html"
-        # email_template_txt  = "mail_body.txt"
-        # text_content = render_to_string(email_template_txt, context)
-        # html_content = render_to_string(email_template_html, context)
+        first_name = applicant.first_name
+        subject = 'InOut Registration approved!'
+        from_email = settings.DEFAULT_FROM_EMAIL
+        to_email = [ applicant.email ]
+        context = {
+            "first_name": first_name,
+        }
+        email_template_html = "mail_body.html"
+        email_template_txt  = "mail_body.txt"
+        text_content = render_to_string(email_template_txt, context)
+        html_content = render_to_string(email_template_html, context)
 
-        # if subject and to_email and from_email :
-        #     try:
-        #         send_mail(subject, text_content, from_email, to_email, fail_silently=False, html_message=html_content)
-        #     except BadHeaderError:
-        #         return HttpResponse('Invalid header found.')
+        if subject and to_email and from_email :
+            try:
+                send_mail(subject, text_content, from_email, to_email, fail_silently=False, html_message=html_content)
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
 
         return HttpResponseRedirect('/admin/inout/inoutuserlink/')
     approve_user.label = "Approve"  # optional
     approve_user.short_description = "Approve user participation"  # optional
 
     objectactions = ('approve_user', )
-
